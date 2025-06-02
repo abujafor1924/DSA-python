b5e38d1d94c8ef629544c597d5f654985ad4bffc
abu-jafor <abujaforwd@yahoo.com>
2025-06-02 15:59:40 +0600

    readmy file added

---
+++ array/array-1.py
@@ -0,0 +1,10 @@
+'''
+An array in Python (via the array module) stores
+elements of the same type efficiently but offers
+limited functionality. A NumPy array (ndarray) is more powerful,
+supporting multi-dimensional data, broadcasting, vectorized operations,
+and numerous mathematical functions, making it ideal for
+scientific computing and data analysis.
+
+'''
+
