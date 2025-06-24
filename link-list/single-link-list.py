8e67d7132ba5ea7333686a5ddd9a65eeefdb9d33
abu-jafor <abujaforwd@yahoo.com>
2025-06-24 11:05:47 +0600

    bugg solv

--- link-list/single-link-list.py
+++ link-list/single-link-list.py
@@ -10,9 +10,23 @@ class LinkList:
         new_node=Node(value)
         self.head=new_node
         self.tail=new_node
-        self.length=1
+        self.length=0
+
+    def append(self,value):
+        new_node=Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.head = new_node
+            self.tail = new_node
+        self.length += 1
+
 
 new_linklist=LinkList(10)
+new_linklist.append(10)
+new_linklist.append(20)
+
 print(new_linklist)
-print(new_linklist.head)
+print(new_linklist.head.value)
 print(new_linklist.tail)
